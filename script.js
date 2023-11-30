c3fdecc384da8653ca6029e14e746c0688619b35
Lucio Araujo <contatolucioaraujo@gmail.com>
2023-11-30 12:51:32 -0300

    added button event

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
 const convertBtn = document.querySelector("#convertBtn")
+const fromOption = document.querySelector("#from-option")
+const toOption = document.querySelector("#to-option")
 
 const baseURL = "https://economia.awesomeapi.com.br/last/"
-const currencyType = "USD-BRL"
 
-const url = `${baseURL}${currencyType}`
+convertBtn.addEventListener('click', async event => {
+    event.preventDefault()
 
-async function getCurrency() {
+    if (fromOption.value === toOption.value) {
+        console.log(1)
+        return 1
+    }
+
+    const currencyType = `${toOption.value}-${fromOption.value}`
+    const url = `${baseURL}${currencyType}`
+
+    console.log(url)
+
     try {
+        const dollar = await getCurrency(url)
+        const ammountToConvert = document.querySelector("#ammount-value").value
+        const result = (ammountToConvert/dollar).toFixed(2)
+        console.log(result)
+    } catch(error) {
+        console.error("Error", error)
+    }
 
+})
+
+
+async function getCurrency(url) {
+    try {
         const response = await fetch(url)
         const data = await response.json()
-
-        console.log(data.USDBRL.bid)
-
+        const dollarValue = data.USDBRL.bid
+        return dollarValue
     } catch(error) {
         console.log(error)
     }
