6caec35b33e42e886d64f56c3a09eeda41376a46
Lucio Araujo <contatolucioaraujo@gmail.com>
2023-12-04 11:31:01 -0300

    fixed conversion bugs

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
 const convertBtn = document.querySelector("#convertBtn")
 const fromOption = document.querySelector("#from-option")
 const toOption = document.querySelector("#to-option")
-
 const baseURL = "https://economia.awesomeapi.com.br/last/"
 
 convertBtn.addEventListener('click', async event => {
     event.preventDefault()
 
     if (fromOption.value === toOption.value) {
-        console.log(1)
         return 1
     }
 
     const currencyType = `${toOption.value}-${fromOption.value}`
     const url = `${baseURL}${currencyType}`
 
-    console.log(url)
-
     try {
-        const dollar = await getCurrency(url)
+        const conversionType = toOption.value+fromOption.value
+        const currency = await getCurrency(url, conversionType)
         const ammountToConvert = document.querySelector("#ammount-value").value
-        const result = (ammountToConvert/dollar).toFixed(2)
-        console.log(result)
+        const result = (ammountToConvert/currency).toFixed(2)
+
+        updateValues(result, ammountToConvert, fromOption.value, toOption.value)
+
     } catch(error) {
         console.error("Error", error)
     }
 
 })
 
-
-async function getCurrency(url) {
+async function getCurrency(url, conversionType) {
     try {
         const response = await fetch(url)
         const data = await response.json()
-        const dollarValue = data.USDBRL.bid
-        return dollarValue
+        const currencyValue = data[conversionType].bid
+
+        console.log(data)
+        return currencyValue 
     } catch(error) {
         console.log(error)
     }
 }
 
+function updateValues(result, ammountToConvert, fromOption, toOption){
+    const fromValue = document.querySelector("#fromValue")
+    const toValue = document.querySelector("#toValue")
 
+    fromValue.innerHTML = new Intl.NumberFormat('en-US', 
+    { style: 'currency', currency: fromOption}).format(ammountToConvert)
+
+    toValue.innerHTML = new Intl.NumberFormat('en-US', 
+    { style: 'currency', currency: toOption}).format(result)
+}
